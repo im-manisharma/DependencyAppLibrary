c6ff8272cf082c50472ae9cbba3638c5ec81c867
im-manisharma <m.sha94rma@gmail.com>
2023-05-15 21:06:41 +0530

    glide added

--- mylibrary/src/main/java/com/example/mylibrary/DependencyActivity.kt
+++ mylibrary/src/main/java/com/example/mylibrary/DependencyActivity.kt
@@ -2,10 +2,13 @@ package com.example.mylibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 
 class DependencyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dependency)
+
+        Glide.get(this)
     }
 }
